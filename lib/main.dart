8169def37ed71c34676c41250a797be80c6d7be7
Ömer Faruk Didin <fdidin59@gmail.com>
2023-10-31 12:50:03 +0300

    burc finished

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: Colors.blue  
       ),
       home: BurcListesi(),
     );
